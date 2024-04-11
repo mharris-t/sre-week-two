60c0931ce82308b6d1ff79afe5989c61f0540878
Munavar Harris Thottoli <60294955+mharris-t@users.noreply.github.com>
2024-04-11 13:26:46 +0000

    add init_env.sh

---
+++ init_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 'Staring Minikube...\n'
+minikube start
+
+echo 'Creating sre K8s namespace...\n'
+kubectl create namespace sre
+
+echo 'Installing Prometheus...\n'
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus -f prometheus.yml --namespace sre
+
+echo 'Installing Grafana...\n'
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install grafana grafana/grafana --namespace sre --set adminPassword="admin"
+
+
